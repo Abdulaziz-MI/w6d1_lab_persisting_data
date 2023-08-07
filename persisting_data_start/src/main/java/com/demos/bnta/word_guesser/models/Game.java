73fea3f2c4b8fae62df019340a8381674fd875b3
Abdulaziz-MI <am.alzaid@outlook.com>
2023-08-07 14:22:26 +0100

    First commit

---
+++ persisting_data_start/src/main/java/com/demos/bnta/word_guesser/models/Game.java
@@ -0,0 +1,54 @@
+package com.demos.bnta.word_guesser.models;
+
+public class Game {
+
+    private static int nextId = 1;
+
+    private int id;
+    private String word;
+    private int guesses;
+    private boolean complete;
+
+    public Game(String word) {
+        this.id = nextId;
+        nextId += 1;
+        this.guesses = 0;
+        this.complete = false;
+        this.word = word;
+    }
+
+    public Game() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getGuesses() {
+        return guesses;
+    }
+
+    public void setGuesses(int guesses) {
+        this.guesses = guesses;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
